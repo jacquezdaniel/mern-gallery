01b24150f146569a414cd751b337dbe03bd6a7d2
Danny <jacquez.j.daniel@gmail.com>
2020-03-13 18:10:45 -0700

    npm

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use(cookieParser());
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
